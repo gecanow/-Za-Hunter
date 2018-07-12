cf8e144553558b882c113729fa9bade13e7406a0
Gaby Ecanow <gecanow@gmail.com>
2018-07-11 20:38:40 -0500

    Added segue to LocationDetailsViewController

---
+++ 'Za Hunter/LocationDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LocationDetailsViewController.swift
+//  'Za Hunter
+//
+//  Created by Necanow on 7/11/18.
+//  Copyright © 2018 EcaKnowGames. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
